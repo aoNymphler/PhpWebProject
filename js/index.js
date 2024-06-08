802e13cb025487cf9d5ec433f3a0e693e807aee9
Efe <efekanefe63186@gmail.com>
2024-06-08 03:55:11 +0300

    son

--- js/index.js
+++ js/index.js
@@ -34,3 +34,60 @@ $('.center-slider').owlCarousel({
 
 
 
+
+$('.bigcenter-slider').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplyHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:1
+        },
+        1000:{
+            items:1
+        }   
+    }
+
+})
+
+
+
+$('.bigcenter2-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplyHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+
+})
+
+
+
+
+
